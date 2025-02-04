57d76c53c35b81abe90ac9a2385e2a46c603a378
faieqlano <ifaieq12@gmail.com>
2025-02-04 17:57:21 +0800

    deploy fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true
+  },
+  webpack: (config: any, { isServer }: { isServer: boolean }) => {
+    // Exclude the functions directory from compilation
+    config.externals = [...(config.externals || []), 'firebase-functions'];
+    return config;
   }
 };
 
